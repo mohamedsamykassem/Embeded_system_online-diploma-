0ab69fa30ecade82528e94301d39ca5298ece5de
mohamed samy <samy69325@gmail.com>
2023-03-12 15:05:08 +0200

    add 2 files in this file

---
+++ C_PROGRAMIG/c assignments/lect3_assignment/main.c
@@ -0,0 +1,21 @@
+/*
+ * main.c
+ *
+ *  Created on: Jan 31, 2023
+ *      Author: original
+ */
+#include "stdio.h"
+void main(){
+	int i,j ;
+	for(i=0 ; i<10 ; i++)
+	{
+
+		for(j=i ; j<10 ; j++)
+		{
+			printf("%d",j);
+		}
+		printf("\n "); // to print line
+
+
+	}
+}
